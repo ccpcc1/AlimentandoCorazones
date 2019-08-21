53ef5a196654549a852f6eccd6e93c0451e75c58
sebastián <sebascz97@gmail.com>
2019-08-20 22:08:36 -0500

    vista inicio de sesion

---
+++ AlimentandoCorazones/www/js/app.js
@@ -0,0 +1,22 @@
+var app = new Framework7({
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'AlimentandoCorazones',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/login/',
+      url: 'index.html',
+    },
+  ],
+  // ... other parameters
+});
+
+var mainView = app.views.create('.view-main');
