5b962b4bd0d82185e2d9a3e43b4b5138cd79a29a
sebastián <sebascz97@gmail.com>
2019-09-03 23:48:48 -0500

    adelanto funcionalidad consultar donaciones

--- Alimentando-corazones/www/js/donaciones.js
+++ Alimentando-corazones/www/js/donaciones.js
@@ -6,14 +6,22 @@ function consultarDonaciones()
 {
     refDonaciones.on("child_added", function(snapshot, prevChildKey) 
     {   
-    //var donacion = snapshot.val();
+    	var donacion = snapshot.val();
         console.log(snapshot.val());
-        console.log(snapshot.key());
-        //VisualizarDonacion(donacion);
+        console.log(snapshot);
+        visualizarDonacion(donacion);
     });
 }
 
-function VisualizarDonacion(donacion)
-{
+function visualizarDonacion(donacion)
+{ 
         // funcion que mostrara los primeros detalles de la donador, direccion
+        console.log(donacion);
+        $("#DonationsContainer").append("\
+        				<div clas='card'>\
+        					<div class='card-header'> PRODUCTO: "+donacion.productos[0].cantidad+" "+donacion.productos[0].unidad+" de "+donacion.productos[0].producto+"</div>\
+                       		<div class='card-content card-content-padding'><h5>CorreoDonador:"+donacion.email+"</h5><h5>Empresa:"+donacion.Empresa+"</h5><h5>Horario de atencion:"+donacion.Horario+"</h5><h5>fechaCaducacion:"+donacion.fechaCaducacion+"</h5>         <h5>Contacto:"+donacion.telefono+"</h5><h5>anotaciones:"+donacion.anotaciones+"</h5></div>\
+                       		<div class='card-footer'>UBICACIÓN: "+donacion.Dirección+"</div>\
+                     </div>");  
+      
 }
--- Alimentando-corazones/www/js/funciones.js
+++ Alimentando-corazones/www/js/funciones.js
@@ -1,9 +1,19 @@
 function interfazRegistro() {
 login.className=" ocultar";
-register.className=" list form-store-data  animated pulse ";	
+register.className=" list form-store-data  animated slideInUp ";	
 }
 
 function interfazLogin() {
 register.className=" ocultar";
-login.className=" animated pulse ";	
+login.className=" animated slideInUp ";	
+}
+
+function interfazBuscarDonaciones() {
+beneficiario.className=" ocultar";
+buscarD.className=" page animated slideInRight ";	
+}
+
+function interfazBeneficiario() {
+beneficiario.className=" page animated slideInLeft";
+buscarD.className=" ocultar ";	
 }
