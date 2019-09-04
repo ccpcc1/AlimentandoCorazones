c1bee17337c7e8f135a3e6fc34fbbd7245cb0f86
sebastián <sebascz97@gmail.com>
2019-09-03 20:40:45 -0500

    perfil de beneficiario con card

--- Alimentando-corazones/platforms/android/app/src/main/assets/www/js/funciones.js
+++ Alimentando-corazones/platforms/android/app/src/main/assets/www/js/funciones.js
@@ -2,3 +2,8 @@ function interfazRegistro() {
 login.className=" ocultar";
 register.className=" list form-store-data  animated pulse ";	
 }
+
+function interfazLogin() {
+register.className=" ocultar";
+login.className=" animated pulse ";	
+}
